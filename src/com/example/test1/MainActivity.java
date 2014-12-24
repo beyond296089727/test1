3f0c3f37d719155b3253f0cbf10093e4c44ead6c
Administrator <296089727@qq.com>
2014-12-24 15:57:25 +0800

    在main类里添加Log.v("第一次修改", "在这里添加点东西");

--- src/com/example/test1/MainActivity.java
+++ src/com/example/test1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v("第一次修改", "在这里添加点东西");
 	}
 
 	@Override
